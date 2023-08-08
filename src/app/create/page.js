56c8af5c59ac9e7e5eefbada6c9b3288ace162de
eejungee <sulau861@gmail.com>
2023-08-08 19:20:00 +0900

    ADD: 3일차 코드 추가

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -19,12 +19,12 @@ export default function Create() {
           body: JSON.stringify({ title, body }),
         };
 
-        fetch(`http://localhost:9999/topics`, options)
+        fetch(process.env.NEXT_PUBLIC_API_URL + `topics`, options)
           .then((res) => res.json())
           .then((result) => {
             console.log(result);
             const lastId = result.id;
-            // router를 통해 방금 생성한 글로 리디렉션 가능
+            router.refresh();
             router.push(`/read/${lastId}`);
           });
       }}
